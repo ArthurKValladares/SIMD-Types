6411f7b0088d529aa467e37a9131d5f2d1311065
ArthurValladares <arthurkaukal@gmail.com>
2024-02-05 18:41:58 -0800

    sketching out basic types

---
+++ src/math/vec4f32.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifdef __ARM_NEON
+#include "sse2neon.h"
+#else
+#include <emmintrin.h>
+#endif
+
+class Vec4i32;
+struct Vec4f32 {
+    Vec4f32();
+    Vec4f32(float val);
+    Vec4f32(float a, float b, float c, float d);
+    Vec4f32(__m128 a);
+    
+    Vec4i32 to_int_nearest() const;
+    Vec4i32 to_int_round_down() const;
+
+    Vec4f32 normalized() const;
+    Vec4f32 min(float val) const;
+    Vec4f32 max(float val) const;
+    Vec4f32 clamp(float min, float max) const;
+    Vec4f32 modf1() const;
+
+    float x() const;
+	float y() const;
+	float z() const;
+	float w() const;
+    
+    float dot(const Vec4f32& rhs) const;
+
+    float operator[](int index) const;
+
+    Vec4f32 operator+(const Vec4f32& rhs) const;
+    Vec4f32 operator*(const Vec4f32& rhs) const;
+    Vec4f32 operator/(const Vec4f32& rhs) const;
+private:
+    union { 
+        __m128 _mf;
+        float _xyzw[4];
+    };
+};
---
+++ src/math/vec4i32.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#ifdef __ARM_NEON
+#include "sse2neon.h"
+#else
+#include <emmintrin.h>
+#endif
+
+struct Vec4f32;
+struct Vec4i32 {
+    Vec4i32();
+    Vec4i32(int a, int b, int c, int d);
+    Vec4i32(int a);
+    Vec4i32(__m128i a);
+    
+    Vec4f32 to_float() const;
+    
+    i32 x() const;
+	i32 y() const;
+	i32 z() const;
+	i32 w() const;
+
+    bool any_gte(int val) const;
+    
+    int operator[](int index) const;
+
+    Vec4i32 operator*(const int& rhs) const;
+
+    Vec4i32 operator+(const Vec4i32& rhs) const;
+    Vec4i32 operator-(const Vec4i32& rhs) const;
+    Vec4i32 operator*(const Vec4i32& rhs) const;
+    
+    void operator+=(const Vec4i32& rhs);
+    void operator-=(const Vec4i32& rhs);
+    void operator*=(const Vec4i32& rhs);
+    
+    Vec4i32 operator&(const Vec4i32& rhs) const;
+    Vec4i32 operator|(const Vec4i32& rhs) const;
+    Vec4i32 operator^(const Vec4i32& rhs) const;
+    Vec4i32 operator!() const;
+    
+    Vec4i32 operator==(const Vec4i32& rhs) const;
+    Vec4i32 operator>(const Vec4i32& rhs) const;
+    Vec4i32 operator<(const Vec4i32& rhs) const;
+    Vec4i32 operator>=(const Vec4i32& rhs) const;
+    Vec4i32 operator<=(const Vec4i32& rhs) const;
+private:
+    union {
+        __m128i _mi;
+        int _xyzw[4];
+    };
+};
